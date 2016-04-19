73ab2c4c51920be4bc8e30830ed60f325ea47440
map <map@qutke.com>
2016-04-19 13:11:22 +0800

    edit socket

--- server/socket.js
+++ server/socket.js
@@ -5,50 +5,70 @@ var dns = require("dns");
 var db = require("./db");
 
 module.exports = function (socket) {
+    var task = {};
     socket.on("DOMAIN", function (data) {
         var url1 = data.url.toString() || "";
-        var url2 = {};
         parseURL(url1).forEach(function (rawUrl) {
-            var pos = 0;
-            var t = +new Date();
-            url2[rawUrl] = [];
-
             socket.emit("DOMAIN_INFO", {domain: rawUrl, msg: "parse started"});
             db.urldb.find({domain: rawUrl}).select("-_id").exec(function (err, doc) {
                 if (!err) {
                     if (!doc.length) {
-                        dnsLook(dicGroup, socket, rawUrl, 0);
+                        if (!task[rawUrl]) {
+                            task[rawUrl] = {
+                                pos: 0,
+                                t: +new Date(),
+                                domain: []
+                            };
+                            dnsLook(dicGroup, socket, rawUrl, 0);
+                        }
+                        else  if (!task[rawUrl].timer){
+                                task[rawUrl].timer = setInterval(function () {
+                                    if (task[rawUrl].pos != dic.length) {
+                                        socket.emit("DOMAIN_DATA", task[rawUrl].domain);
+                                        socket.emit("DOMAIN_PROGRESS", {
+                                            domain: rawUrl,
+                                            pos: task[rawUrl].pos / dic.length
+                                        })
+                                    }
+                                    else {
+                                        clearInterval(task[rawUrl].timer);
+                                        socket.emit("DOMAIN_PROGRESS", {domain: rawUrl, pos: 1})
+                                    }
+                                }, 1000)
+                        }
                     }
                     else {
                         socket.emit("DOMAIN_DATA", doc[0].subdomain);
                         socket.emit("DOMAIN_PROGRESS", {domain: rawUrl, pos: 1});
                     }
                 }
-                else  socket.emit("DOMAIN_ERROR", {domain: rawUrl, msg: "db rong"});
+                else  socket.emit("DOMAIN_ERROR", {domain: rawUrl, msg: "db wrong"});
             });
+
             function dnsLook(arr, socket, rawUrl, i) {
                 arr[i].forEach(function (item, index) {
                     dns.lookup([item, rawUrl].join("."), 4, function (err, doc) {
-                        pos++;
+                        task[rawUrl].pos++;
                         if (!err && !/202\.106\.199/.test(doc)) {
                             var data = {ip: doc, url: item, domain: rawUrl};
                             socket.emit("DOMAIN_DATA", [data]);
-                            url2[rawUrl].push(data)
+                            task[rawUrl].domain.push(data)
                         }
                         if (index + 1 == arr[i].length && i + 1 < arr.length) {
                             dnsLook(arr, socket, rawUrl, i + 1);
                         }
-                        if (+new Date() - t > 1000) {
-                            t = +new Date();
-                            socket.emit("DOMAIN_PROGRESS", {domain: rawUrl, pos: pos / dic.length})
+                        if (+new Date() - task[rawUrl].t > 1000) {
+                            task[rawUrl].t = +new Date();
+                            socket.emit("DOMAIN_PROGRESS", {domain: rawUrl, pos: task[rawUrl].pos / dic.length})
                         }
-                        if (pos == dic.length) {
+                        if (task[rawUrl].pos == dic.length) {
                             socket.emit("DOMAIN_PROGRESS", {domain: rawUrl, pos: 1});
 
                             db.urldb.findOneAndUpdate({domain: rawUrl}, {
-                                subdomain: url2[rawUrl],
+                                subdomain: task[rawUrl].domain,
                                 updated_at: Date.now()
                             }, {upsert: true}, function () {
+                                delete task[rawUrl];
                             })
                         }
                     });
@@ -69,7 +89,7 @@ function arrSlice(arr, len) {
     var r = [];
     for (var i = 0; i < len; i++)
         r.push(arr.slice(i * d, (i + 1) * d))
-    r[r.length-1]= r[r.length-1].concat(arr.slice(-le));
+    r[r.length - 1] = r[r.length - 1].concat(arr.slice(-le));
     return r;
 }
 
